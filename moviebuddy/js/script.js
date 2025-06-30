c178b666930357b8e52002532069c847153b650f
kevindam07 <kevindamasu2007@gmail.com>
2025-06-30 10:33:45 +0800

    Delete moviebuddy directory

--- moviebuddy/js/script.js
+++
@@ -1,9 +0,0 @@
-  document.addEventListener("DOMContentLoaded", function() {
-      var typed = new Typed(".typing-text", {
-          strings: ["Comedy", "Action", "Drama", "Horror", "Sci-Fi", "Romance", "Thriller", "Adventure"],
-          loop: true,
-          typeSpeed: 50,
-          backSpeed: 25,
-          backDelay: 500,
-      });
-  });
